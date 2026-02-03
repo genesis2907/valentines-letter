092cdeab2f489d32d09f57e96d4dd7f7df298eca
Synkee <aksheymani@gmail.com>
2026-02-03 19:07:57 +0800

    Rendition 7

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("click", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Yayyyyyy!!!";
 
     catImg.src = "cat_dance.gif";
 
